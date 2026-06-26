Kevin-Li-2025/qdrant

[ai] Refactor `OwnedReadPipeline` as generic wrapper over `BorrowedReadPipeline` impl

lib/common/common/src/universal_io/mod.rs
@@ -21,7 +21,7 @@ pub use self::simple_disk_cache::{
     DiskCache, DiskCacheConfig, DiskCacheFs, DiskCacheFsContext, DiskCacheRemote,
 };
 pub use self::traits::{
-    BorrowedReadPipeline, Item, OpenExtra, OwnedReadPipeline, UniversalRead, UniversalReadFileOps,
+    Item, OpenExtra, OwningPipeline, ReadPipelineImpl, UniversalRead, UniversalReadFileOps,
     UniversalReadFs, UniversalWrite, UserData,
 };
 pub use self::types::{

lib/common/common/src/universal_io/traits/mod.rs
@@ -6,7 +6,7 @@ mod write;
 
 pub use file_ops::{UniversalReadFileOps, UniversalReadFs};
 pub use open_extra::OpenExtra;
-pub use pipeline::{BorrowedReadPipeline, OwnedReadPipeline};
+pub use pipeline::{OwningPipeline, ReadPipelineImpl};
 pub use read::UniversalRead;
 pub use write::UniversalWrite;
 

lib/common/common/src/universal_io/traits/read.rs
@@ -2,7 +2,7 @@ use std::borrow::Cow;
 use std::fmt::Debug;
 use std::ops::Range;
 
-use super::{BorrowedReadPipeline, Item, OwnedReadPipeline, UniversalReadFs, UserData};
+use super::{Item, ReadPipelineImpl, UniversalReadFs, UserData};
 use crate::ext::aligned_vec::ACow;
 use crate::generic_consts::{AccessPattern, Sequential};
 use crate::universal_io::{ReadBytesItem, ReadRange, Result, UniversalKind};
@@ -42,13 +42,14 @@ pub trait UniversalRead: Sized + Debug + Send + Sync {
     /// constructor still accepts the unwrapped inner `S::Fs`.
     type Fs: UniversalReadFs<File = Self>;
 
-    type BorrowedReadPipeline<'file, U>: BorrowedReadPipeline<'file, U, File = Self>
+    /// The single read-pipeline implementation for this backend.
+    ///
+    /// The *owned* (file-owning) pipeline is derived generically as
+    /// [`OwningPipeline<Self::ReadPipeline<'static, U>, Self, U>`](super::OwningPipeline);
+    /// backends do not implement it separately.
+    type ReadPipeline<'file, U>: ReadPipelineImpl<'file, U, File = Self>
     where
         Self: 'file,
-        U: UserData;
-
-    type OwnedReadPipeline<U>: OwnedReadPipeline<U, File = Self>
-    where
         U: UserData;
 
     /// Enables live-reloading of files. Append-only files can make the
@@ -124,7 +125,7 @@ pub trait UniversalRead: Sized + Debug + Send + Sync {
         P: AccessPattern,
         U: UserData,
     {
-        let mut pipeline = Self::BorrowedReadPipeline::<'_, U>::new()?;
+        let mut pipeline = Self::ReadPipeline::<'_, U>::new()?;
         let mut ranges = ranges.into_iter();
 
         Ok(std::iter::from_fn(move || {
@@ -187,7 +188,7 @@ pub trait UniversalRead: Sized + Debug + Send + Sync {
         U: UserData,
         Self: 'a,
     {
-        let mut pipeline = Self::BorrowedReadPipeline::<'a, U>::new()?;
+        let mut pipeline = Self::ReadPipeline::<'a, U>::new()?;
         let mut reads = reads.into_iter();
 
         let iter = std::iter::from_fn(move || {

viewport-corp/fork-qdrant

lib/common/common/src/universal_io/mod.rs
@@ -21,7 +21,7 @@ pub use self::simple_disk_cache::{
     DiskCache, DiskCacheConfig, DiskCacheFs, DiskCacheFsContext, DiskCacheRemote,
 };
 pub use self::traits::{
-    BorrowedReadPipeline, Item, OpenExtra, OwnedReadPipeline, UniversalRead, UniversalReadFileOps,
+    Item, OpenExtra, OwnedPipeline, ReadPipeline, UniversalRead, UniversalReadFileOps,
     UniversalReadFs, UniversalWrite, UserData,
 };
 pub use self::types::{

lib/common/common/src/universal_io/traits/mod.rs
@@ -6,7 +6,7 @@ mod write;
 
 pub use file_ops::{UniversalReadFileOps, UniversalReadFs};
 pub use open_extra::OpenExtra;
-pub use pipeline::{BorrowedReadPipeline, OwnedReadPipeline};
+pub use pipeline::{OwnedPipeline, ReadPipeline};
 pub use read::UniversalRead;
 pub use write::UniversalWrite;
 

lib/common/common/src/universal_io/traits/read.rs
@@ -2,7 +2,7 @@ use std::borrow::Cow;
 use std::fmt::Debug;
 use std::ops::Range;
 
-use super::{BorrowedReadPipeline, Item, OwnedReadPipeline, UniversalReadFs, UserData};
+use super::{Item, ReadPipeline, UniversalReadFs, UserData};
 use crate::ext::aligned_vec::ACow;
 use crate::generic_consts::{AccessPattern, Sequential};
 use crate::universal_io::{ReadBytesItem, ReadRange, Result, UniversalKind};
@@ -42,13 +42,14 @@ pub trait UniversalRead: Sized + Debug + Send + Sync {
     /// constructor still accepts the unwrapped inner `S::Fs`.
     type Fs: UniversalReadFs<File = Self>;
 
-    type BorrowedReadPipeline<'file, U>: BorrowedReadPipeline<'file, U, File = Self>
+    /// The single read-pipeline implementation for this backend.
+    ///
+    /// The *owned* (file-owning) pipeline is derived generically as
+    /// [`OwnedPipeline<Self::ReadPipeline<'static, U>, Self, U>`](super::OwnedPipeline);
+    /// backends do not implement it separately.
+    type ReadPipeline<'file, U>: ReadPipeline<'file, U, File = Self>
     where
         Self: 'file,
-        U: UserData;
-
-    type OwnedReadPipeline<U>: OwnedReadPipeline<U, File = Self>
-    where
         U: UserData;
 
     /// Enables live-reloading of files. Append-only files can make the
@@ -124,7 +125,7 @@ pub trait UniversalRead: Sized + Debug + Send + Sync {
         P: AccessPattern,
         U: UserData,
     {
-        let mut pipeline = Self::BorrowedReadPipeline::<'_, U>::new()?;
+        let mut pipeline = Self::ReadPipeline::<'_, U>::new()?;
         let mut ranges = ranges.into_iter();
 
         Ok(std::iter::from_fn(move || {
@@ -190,7 +191,7 @@ pub trait UniversalRead: Sized + Debug + Send + Sync {
         U: UserData,
         Self: 'a,
     {
-        let mut pipeline = Self::BorrowedReadPipeline::<'a, U>::new()?;
+        let mut pipeline = Self::ReadPipeline::<'a, U>::new()?;
         let mut reads = reads.into_iter();
 
         let iter = std::iter::from_fn(move || {